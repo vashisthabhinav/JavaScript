7a4ba0a09adc4b39a78693fbb5c72e8bcbb2171f
Abhinav Vashisth <89785800+vashisthabhinav@users.noreply.github.com>
2022-05-23 21:26:52 +0530

    Update scripts.js

--- JS Udemy/js/scripts.js
+++ JS Udemy/js/scripts.js
@@ -608,3 +608,244 @@ count = 20;
 do{
     console.log(count++);
 } while(count<5);
+
+
+// *****************************
+// L - 21 
+// V27,28 - Conditionals
+// *****************************
+
+// Conditionals are used to determine if a block of code should or should not be executed depending on a given set of conditions.
+var age = 17;
+
+if (age < 18) {
+
+    console.log('Underage');
+
+} else {
+
+    console.log('Overage');
+
+}
+
+// The console will show: 'Underage'
+
+
+var age = 50;
+
+if (age < 18) {
+
+    console.log('Underage');
+
+} else if (age >= 65) {
+
+    console.log('Senior');
+
+} else {
+
+    console.log('Adult');
+
+}
+
+// The console will show: 'Senior'  
+
+// Using Ternary if:
+console.log(age<18?"Underage":age>=65?"Senior":"Adult")
+
+/*
+Testing multiple conditions at once with and / or
+    && (and): when we use &&, all conditions need to be true so the the code is executed
+    || (or): when we use ||, we just need one condition to be true to execute the code
+*/
+
+grade = 8;
+absences = 4;
+
+// Solving the problem with && (and): 
+
+if (grade >= 7 && absences <= 5) {
+    console.log( 'The student has been approved' );
+} else {
+    console.log( 'The student has failed' );
+}
+
+// Solving the problem with || (or):
+
+if (grade < 7 || absences > 5) {
+    console.log( 'The student has failed' );
+} else {
+    console.log( 'The student has been approved' );
+}
+
+// Both ways are correct
+
+/*
+Evaluating values as conditions
+    In Javascript, just like Python, PHP etc., we can evaluate values as true or false without booleans. 
+    Javascript considers to be false:
+        false
+        undefined
+        null
+        0
+        NaN
+        "" (empty string)
+    All other values, numbers, strings, objects and arrays are considered to be true.
+*/
+var name = "";
+
+// if (name) {
+    
+//     console.log("The name is " + name);
+
+// } else {
+
+//     console.log("The name has not been informed");
+
+// }
+
+// The console will show "The name has not been informed"
+
+// Ternary if - another way of writing conditionals using the ternary notation.
+
+console.log(name ? "The name is " + name:"The name has not been informed.");
+
+
+// *****************************
+// L - 22 
+// V29 - Conditionals
+// *****************************
+
+/*
+Nested loops and conditionals
+    When we use loops inside loops or conditionals inside conditionals, this is called nesting. 
+*/
+
+var isMember = true;
+    var age = 25;
+if(isMember||age>65){
+    console.log("The product is free");
+}
+else{
+    if(age<18){
+        console.log("The price of the product is $6")
+    }else{
+        console.log("The price of the product is $12")
+    }
+}
+
+// Another method without the nesting
+if(isMember||age>65){
+    console.log("The product is free");
+}else if (age>18){
+    console.log("The price of the product is $6");
+}else{
+    console.log("The price of the product is $12")
+}
+
+
+var employees = [
+        
+    {
+        'name': 'Charles Silva',
+        'age': 45,
+        'children': ['Andrew Silva', 'Maria Silva','Philipp Banks', 'Rose Banks', 'Tara Banks']
+        
+    },
+
+    {
+        'name': 'Elizabeth Green',
+        'age': 32,
+        'children': ['Peter Green']
+        
+    },
+
+    {
+        'name': 'George Banks',
+        'age': 39,
+        'children': ['Philipp Banks', 'Rose Banks', 'Tara Banks']
+        
+    }
+
+];
+
+document.getElementById("children").innerHTML = "";
+for(var i=0;i<employees.length;i++){
+    var childrenList = employees[i].children;
+    for(var b=0; b<childrenList.length; b++){
+        var child = childrenList[b];
+        // console.log(child);
+        document.getElementById("children").innerHTML += "<li>" + child + "</li>";// += sent to concatenate the string
+    }
+}
+
+
+// *****************************
+// L - 23 
+// V30 - Conditionals
+// *****************************
+
+
+/*
+Variable scope defines the accessibility of a variable from different parts of the code. 
+There are two main scopes in Javascript: Global and Local.
+    Local scope
+        Functions in Javascript have their own local scope. 
+    This means that variables created inside functions cannot be accessed from outside the function.
+
+    Global Scope
+        Variables created outside functions belong to the global scope and can be accessed from anywhere in the code, including from inside functions.
+    
+    Variables created without the var keyword also belong to the global scope, even if they were created inside functions.
+    This is something that should only be done intentionally as it can result in errors in your program that can be hard to spot.
+*/
+
+function create_name() {
+    var personsName = 'Mary';// var keyword is the local variable
+    // If var is not used, it will be in a global scope
+    console.log(personsName);
+}
+
+create_name(); // The console will show 'Mary'
+
+console.log(personsName); // The console will show an error because 
+// we don't have access so the name variable.
+// So, if we don't want any errors, we have to define a var called personsName outside of the function i.e., in the global scope.
+// Functions are only accessible from the inside.
+
+var personsName = "John";
+if(true){
+    var personsName = "Mary";
+    console.log(personsName)
+}
+console.log(personsName)
+//The if here will be able to replace the values of the personsName variable from teh inside of the loop
+// This is because the conditionals have global scope.
+// Any function created here is the part of the glocal scope.
+
+for(var i = 0; i<3;i++){
+    console.log("Parent loop: ", i)
+
+    for(var i = 0; i<3;i++){
+        // The naming of the variable in the parent and the child loop should be different because it can create problems here.
+        // Like in this situation, when i is 0, the first parent console runs and then the child loops run till i turns 3 and only 0 is printed for the parent loop and both the loops exit. 
+        console.log("Child loop: ", i)
+    }
+}
+
+console.log("*******************");
+
+// In order to deal with this problem, we use let keyword which always creates a local variable which can be used inside the any block of code that is inside curly braces.
+for(let i = 0; i<3;i++){
+    console.log("Parent loop: ", i)
+
+    for(let i = 0; i<3;i++){
+        // The naming of the variable in the parent and the child loop should be different because it can create problems here.
+        // Like in this situation, when i is 0, the first parent console runs and then the child loops run till i turns 3 and only 0 is printed for the parent loop and both the loops exit. 
+        console.log("Child loop: ", i)
+    }
+}
+
+// const cannot be changed
+
+const PI = 3.1415;
+// PI = 0;//This will give an error
